9997ec59cb5c3d7a76f93efd6f5d57331b843991
Phạm Nguyên Cát <dtpham258@gmail.com>
2020-01-16 11:06:31 +0700

    add call with context

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/micro/examples v0.2.0
 	github.com/micro/go-micro v1.18.0
-	github.com/onskycloud/rbac v1.1.8
+	github.com/onskycloud/rbac v1.2.5
 )
--- main.go
+++ main.go
@@ -46,6 +46,30 @@ func CallMicro(ns string, svcName string, endPoint string, body string, result i
 	return nil
 }
 
+// CallWithContext cal micro no protos with context
+func CallWithContext(ctx context.Context, ns string, svcName string, endPoint string, body string, result interface{}) error {
+	service := k8s.NewService()
+	service.Init()
+	c := service.Client()
+	t := StrToMap(body)
+	request := c.NewRequest(fmt.Sprintf("%s.%s", svcName, ns), endPoint, t, client.WithContentType("application/json"))
+	var response json.RawMessage
+
+	if err := c.Call(ctx, request, &response); err != nil {
+		return err
+	}
+	// raw := StrToMap(string(response))
+	raw := string(response)
+	if raw != "" {
+		// err := mapstructure.Decode(raw, &result)
+		err := json.Unmarshal([]byte(raw), result)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // StrToMap string to map
 func StrToMap(in string) map[string]interface{} {
 	var result map[string]interface{}
